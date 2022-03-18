58be859d3b5d01c78a73243670386d34eb7d86a5
Tymofii Lanevych <tymofii.lanevych@plus4u.net>
2022-03-18 09:57:32 +0200

    init commit

---
+++ src/main/java/com/timak31/wallet/TlWalletBotApplication.java
@@ -0,0 +1,13 @@
+package com.timak31.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlWalletBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TlWalletBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timak31/wallet/TlWalletBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timak31.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TlWalletBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
